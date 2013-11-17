38d9956b7ffbe29919d86eab362ca8dd5b182b53
Bogdan Vazzolla <bogdan.vazzolla@gmail.com>
2013-11-18 00:06:34 +0200

    Adding new structure to the project - better to understand

--- web.js
+++ web.js
@@ -1,33 +1,56 @@
 var express = require("express");
 var app = express();
-app.use(express.logger());
-app.use(express.bodyParser());
-app.use('/client', express.static(__dirname + '/client'));
+var http = require('http');
+var app = express();
+var socket_io = require('socket.io');
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+// catch all exception for saving the node.js server from crashing
+// disable when debugging if the information logged is not enough
+// and let the server crash
+process.on('uncaughtException', function (err) {
+    console.error(err);
+    console.log("Node NOT Exiting...");
 });
 
-app.get('/hello.txt', function(req, res){
-  var body = 'Hello World';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
-  res.end(body);
+// configuration of the node application
+var appPort = 5000;
+var appHost = 'localhost';
+
+
+
+app.configure(function(){
+    app.set('port', appPort);
+    app.set('views', __dirname + '/app/server/views');
+    app.set('view engine', 'jade');
+    app.locals.pretty = true;
+    app.locals.apphost = appHost;
+    app.locals.appport = appPort;
+    //app.use(express.favicon(__dirname + '/app/public/img/favicon.ico'));
+
+    app.use(express.logger('development'));
+    app.use(express.bodyParser());
+    app.use(express.cookieParser());
+    app.use(express.methodOverride());
+    app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
+    app.use(express.static(__dirname + '/app/public'));
 });
 
-app.post('/msg', function(req, res){
-  var message = req.body.message;
-  if (message){
-    // echo back the message
-    res.send(200, {'message': message })
-  }
-  else{
-    // bad request
-    res.send(400)
-  }
-})
 
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.configure('development', function(){
+    app.use(express.errorHandler());
 });
+
+app.configure('production', function(){
+    //code minified and compress for production
+});
+
+require('./app/server/router')(app);
+
+http.createServer(app.handle.bind(app)).listen(app.get('port'));
+
+
+
+app.use('/public', express.static(__dirname + '/public'));
+
+
